c32a98221d2a9120f0723c96233c11e922f8a364
Nipun Marak <26696002+nipun95@users.noreply.github.com>
2018-10-30 02:09:44 +0600

    Create RotateActor.cpp

---
+++ RotateActor.cpp
@@ -0,0 +1,30 @@
+#include "RotateActor.h"
+
+
+// Sets default values
+ARotateActor::ARotateActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PitchValue = 0.f;
+	YawValue = 0.f;
+	RollValue = 0.f;
+}
+
+// Called when the game starts or when spawned
+void ARotateActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARotateActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FQuat QuatRotation = FQuat(FRotator(PitchValue, YawValue, RollValue));
+	AddActorLocalRotation(QuatRotation, false, 0, ETeleportType::None);
+}
+

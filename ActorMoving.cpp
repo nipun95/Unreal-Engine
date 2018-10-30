948887742263a39d99353859407d08a3eed60abb
Nipun Marak <26696002+nipun95@users.noreply.github.com>
2018-10-31 01:54:43 +0600

    Create ActorMoving.cpp

---
+++ ActorMoving.cpp
@@ -0,0 +1,43 @@
+#include "KillingActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+
+
+// Sets default values
+AKillingActor::AKillingActor()
+{
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	tRoot = CreateDefaultSubobject<USphereComponent>(TEXT("Root"));
+
+	MyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyMesh"));
+	MyMesh->AttachTo(tRoot);
+
+	SpeedScale = 00.0f;
+}
+
+// Called when the game starts or when spawned
+void AKillingActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AKillingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation();
+	DeltaMove = (FMath::Sin(Runtime + DeltaTime) - FMath::Sin(Runtime));
+	if(MovetoXAxis)
+		NewLocation.Y += DeltaMove * SpeedScale;
+	else
+		NewLocation.X += DeltaMove * SpeedScale;
+	Runtime += DeltaTime;
+	SetActorLocation(NewLocation);
+}
+};

bdb23e9d0528c03aa21b382e00246dfa8f838aa3
Nipun Marak <26696002+nipun95@users.noreply.github.com>
2018-11-01 04:50:19 +0600

    Add files via upload

---
+++ ActorPickUp.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ActorPickUp.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AActorPickUp::AActorPickUp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Speed = 1.0f;
+	
+
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider"));
+	//BoxCollider->bGenerateOverlapEvents = true;
+	BoxCollider->OnComponentBeginOverlap.AddDynamic(this, &AActorPickUp::OnBeginOverlap);
+	RootComponent = BoxCollider;
+	BoxCollider->SetWorldScale3D(FVector(0.5f, 0.5f, .015f));
+	MyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Mesh"));
+	MyMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+		
+}
+
+void AActorPickUp::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
+	{
+		FString pickup = FString::Printf(TEXT("Picked up: %s"), *GetName());
+		GEngine->AddOnScreenDebugMessage(1, 5, FColor::Yellow, pickup);
+		Destroy();
+	}
+}
+
+// Called when the game starts or when spawned
+void AActorPickUp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AActorPickUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AddActorLocalRotation(RotationRate * DeltaTime * Speed);
+}
+
---
+++ ActorPickUp.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/ShapeComponent.h"
+#include "ActorPickUp.generated.h"
+
+
+UCLASS()
+class LEARNINGACTOR_API AActorPickUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AActorPickUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadwrite, Category = Pickup, meta = (AllowPrivateAccess = true))
+		UShapeComponent* BoxCollider;
+	UPROPERTY(VisibleAnywhere, BlueprintReadwrite, Category = Pickup, meta = (AllowPrivateAccess = true))
+		UStaticMeshComponent* MyMesh;
+	UPROPERTY(EditAnywhere)
+		float Speed;
+	UPROPERTY(EditAnywhere)
+		FRotator RotationRate = FRotator(100.0f, 0.0f, 0.0f);
+
+	//Overlap function
+	UFUNCTION()
+		void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+			bool bFromSweep, const FHitResult& SweepResult);
+	
+};

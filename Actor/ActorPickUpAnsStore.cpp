495a8594476f1e72bfb07df8a42dda49b5f00094
Nipun Marak <26696002+nipun95@users.noreply.github.com>
2018-11-02 14:35:46 +0600

    Update and rename ActorPickUpAnsStore.cpp to Actor/ActorPickUpAnsStore.cpp

--- Actor/ActorPickUpAnsStore.cpp
+++ Actor/ActorPickUpAnsStore.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "ActorPickUp.h"
+#include "ActorPickUpAnsStore.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/BoxComponent.h"
 
 
 // Sets default values
-AActorPickUp::AActorPickUp()
+AActorPickUpAnsStore::AActorPickUpAnsStore()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -23,14 +23,14 @@ AActorPickUp::AActorPickUp()
 		
 }
 
-void AActorPickUp::OnInteract()
+void AActorPickUpAnsStore::OnInteract()
 {
 	FString pickup = FString::Printf(TEXT("Picked up: %s"), *Name);
 	GEngine->AddOnScreenDebugMessage(1, 5, FColor::Yellow, pickup);
 	Destroy();
 }
 
-void AActorPickUp::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+void AActorPickUpAnsStore::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
 	const FHitResult& SweepResult)
 {
@@ -43,14 +43,14 @@ void AActorPickUp::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* O
 }
 
 // Called when the game starts or when spawned
-void AActorPickUp::BeginPlay()
+void AActorPickUpAnsStore::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void AActorPickUp::Tick(float DeltaTime)
+void AActorPickUpAnsStore::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
